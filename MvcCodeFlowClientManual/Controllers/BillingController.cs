731fe7ca7949c51440440d32f276171dc019bb27
Rohith Pudota <rohithchoudhary_pudota@intuit.com>
2018-04-17 12:35:01 -0700

    Adding comments

--- MvcCodeFlowClientManual/Controllers/BillingController.cs
+++ MvcCodeFlowClientManual/Controllers/BillingController.cs
@@ -38,7 +38,7 @@ namespace MvcCodeFlowClientManual.Controllers
                     Bill bill = CreateBill(serviceContext, vendor);
                     //Create Vendor Credit
                     VendorCredit vendorCredit = CreateVendorCredit(serviceContext, vendor);
-                    //Add BillPayment for this Vendor and apply a vendorCredit
+                    //Add BillPayment for this Vendor and apply a VendorCredit
                     BillPayment billPayment = CreateBillPaymentCreditCard(serviceContext, vendor, bill, vendorCredit);
 
                     return View("Index", (object)("QBO API calls Success!"));
@@ -53,6 +53,10 @@ namespace MvcCodeFlowClientManual.Controllers
                 return View("Index", (object)"QBO API call Failed!");
         }
 
+        /*
+         * Create a Vendor
+         * The Vendor object represents the seller from whom your company purchases any service or product. 
+         */
         private static Vendor CreateVendor()
         {
             Vendor vendor = new Vendor();
@@ -65,84 +69,72 @@ namespace MvcCodeFlowClientManual.Controllers
             billAddr.Line5 = "Line5";
             billAddr.City = "City";
             billAddr.Country = "Country";
-
             billAddr.CountrySubDivisionCode = "CountrySubDivisionCode";
             billAddr.PostalCode = "PostalCode";
 
             vendor.BillAddr = billAddr;
-
             vendor.TaxIdentifier = "TaxIdentifier";
-
             vendor.Balance = new Decimal(100.00);
             vendor.BalanceSpecified = true;
             vendor.OpenBalanceDate = DateTime.UtcNow.Date;
             vendor.OpenBalanceDateSpecified = true;
-
             vendor.AcctNum = "AcctNum";
             vendor.Vendor1099 = true;
             vendor.Vendor1099Specified = true;
-
             vendor.Title = "Title";
             vendor.GivenName = "GivenName";
             vendor.MiddleName = "MiddleName";
             vendor.FamilyName = "FamilyName";
             vendor.Suffix = "Suffix";
-
-
             vendor.CompanyName = "CompanyName";
             vendor.DisplayName = "DisplayName_" + Guid.NewGuid().ToString("N");
             vendor.PrintOnCheckName = "PrintOnCheckName";
-
             vendor.Active = true;
             vendor.ActiveSpecified = true;
+
             TelephoneNumber primaryPhone = new TelephoneNumber();
-
             primaryPhone.FreeFormNumber = "FreeFormNumber";
-
             vendor.PrimaryPhone = primaryPhone;
+
             TelephoneNumber alternatePhone = new TelephoneNumber();
-
             alternatePhone.FreeFormNumber = "FreeFormNumber";
-
             vendor.AlternatePhone = alternatePhone;
+
             TelephoneNumber mobile = new TelephoneNumber();
-
             mobile.FreeFormNumber = "FreeFormNumber";
-
             vendor.Mobile = mobile;
+
             TelephoneNumber fax = new TelephoneNumber();
-
             fax.FreeFormNumber = "FreeFormNumber";
-
             vendor.Fax = fax;
+
             EmailAddress primaryEmailAddr = new EmailAddress();
             primaryEmailAddr.Address = "Address@add.com";
-
             vendor.PrimaryEmailAddr = primaryEmailAddr;
+
             WebSiteAddress webAddr = new WebSiteAddress();
             webAddr.URI = "http://site.com";
-
             vendor.WebAddr = webAddr;
 
             return vendor;
         }
 
+        /*
+         * Create's a Bill
+         * A Bill object is an AP transaction representing a request-for-payment from a third party for goods/services rendered, received, or both. 
+         */
         private static Bill CreateBill(ServiceContext serviceContext, Vendor vendors)
         {
-
             QueryService<Customer> querySvc = new QueryService<Customer>(serviceContext);
             Customer customer = querySvc.ExecuteIdsQuery("SELECT * FROM Customer WHERE CompanyName like 'Amy%'").FirstOrDefault();
-
 
             QueryService<Account> accountQuerySvc = new QueryService<Account>(serviceContext);
             Account account = accountQuerySvc.ExecuteIdsQuery("SELECT * FROM Account WHERE AccountType='Accounts Payable' AND Classification='Liability'").FirstOrDefault();
             Account accountExpense = accountQuerySvc.ExecuteIdsQuery("SELECT * FROM Account WHERE AccountType='Expense' AND Classification='Expense'").FirstOrDefault();
 
             Bill bill = new Bill();
-
             bill.DueDate = DateTime.UtcNow.Date;
             bill.DueDateSpecified = true;
-
             bill.VendorRef = new ReferenceType()
             {
 
@@ -159,10 +151,8 @@ namespace MvcCodeFlowClientManual.Controllers
             bill.TotalAmtSpecified = true;
             bill.Balance = new Decimal(100.00);
             bill.BalanceSpecified = true;
-
             bill.TxnDate = DateTime.UtcNow.Date;
             bill.TxnDateSpecified = true;
-
 
             List<Line> lineList = new List<Line>();
             Line line = new Line();
@@ -170,8 +160,6 @@ namespace MvcCodeFlowClientManual.Controllers
             line.Description = "Description";
             line.Amount = new Decimal(100.00);
             line.AmountSpecified = true;
-
-
             line.DetailType = LineDetailTypeEnum.AccountBasedExpenseLineDetail;
             line.DetailTypeSpecified = true;
 
@@ -181,19 +169,20 @@ namespace MvcCodeFlowClientManual.Controllers
             detail.BillableStatus = BillableStatusEnum.NotBillable;
 
             line.AnyIntuitObject = detail;
-
-
             lineList.Add(line);
             bill.Line = lineList.ToArray();
-
             return bill;
         }
 
+        /*
+         * Creates a VendorCredit 
+         * The VendorCredit object is an accounts payable transaction that represents a refund or credit of payment for goods or services. 
+         * It is a credit that a vendor owes you for various reasons such as overpaid bill, returned merchandise, or other reasons.
+         */
         private static VendorCredit CreateVendorCredit(ServiceContext serviceContext, Vendor vendor)
         {
             QueryService<Account> accountQuerySvc = new QueryService<Account>(serviceContext);
             VendorCredit vendorCredit = new VendorCredit();
-
             vendorCredit.VendorRef = new ReferenceType()
             {
                 name = vendor.DisplayName,
@@ -209,19 +198,14 @@ namespace MvcCodeFlowClientManual.Controllers
 
             vendorCredit.TotalAmt = new Decimal(50.00);
             vendorCredit.TotalAmtSpecified = true;
-
             vendorCredit.TxnDate = DateTime.UtcNow.Date;
             vendorCredit.TxnDateSpecified = true;
 
-
             List<Line> lineList = new List<Line>();
             Line line = new Line();
-
             line.Description = "Description";
             line.Amount = new Decimal(50.00);
             line.AmountSpecified = true;
-
-
             line.DetailType = LineDetailTypeEnum.AccountBasedExpenseLineDetail;
             line.DetailTypeSpecified = true;
 
@@ -230,30 +214,29 @@ namespace MvcCodeFlowClientManual.Controllers
             {
                 AccountRef = new ReferenceType() { name = expenseAccount.Name, Value = expenseAccount.Id }
             };
-
-
             lineList.Add(line);
             vendorCredit.Line = lineList.ToArray();
 
             return vendorCredit;
         }
 
-        internal static BillPayment CreateBillPaymentCreditCard(ServiceContext serviceContext, Vendor vendor, Bill bill, VendorCredit vendorCredit)
+        /*
+         * Create a BillPayment. 
+         * A BillPayment object represents the payment transaction for a bill that the business owner receives from a vendor for goods or services purchased from the vendor. 
+         * QuickBooks Online supports bill payments through a credit card or a checking account. 
+         */
+        private static BillPayment CreateBillPaymentCreditCard(ServiceContext serviceContext, Vendor vendor, Bill bill, VendorCredit vendorCredit)
         {
             QueryService<Account> accountQuerySvc = new QueryService<Account>(serviceContext);
 
             BillPayment billPayment = new BillPayment();
             billPayment.PayType = BillPaymentTypeEnum.Check;
             billPayment.PayTypeSpecified = true;
-
             billPayment.TotalAmt = vendorCredit.TotalAmt;
             billPayment.TotalAmtSpecified = true;
-
             billPayment.TxnDate = DateTime.UtcNow.Date;
             billPayment.TxnDateSpecified = true;
-
             billPayment.PrivateNote = "PrivateNote";
-
             billPayment.VendorRef = new ReferenceType()
             {
                 name = vendor.DisplayName,
@@ -291,9 +274,7 @@ namespace MvcCodeFlowClientManual.Controllers
             billPayment.AnyIntuitObject = billPaymentCreditCard;
 
             List<Line> lineList = new List<Line>();
-
             Line line1 = new Line();
-
             line1.Amount = bill.TotalAmt;
             line1.AmountSpecified = true;
             List<LinkedTxn> LinkedTxnList1 = new List<LinkedTxn>();
@@ -302,28 +283,20 @@ namespace MvcCodeFlowClientManual.Controllers
             linkedTxn1.TxnType = TxnTypeEnum.Bill.ToString();
             LinkedTxnList1.Add(linkedTxn1);
             line1.LinkedTxn = LinkedTxnList1.ToArray();
-
             lineList.Add(line1);
-
             Line line = new Line();
-
-
             line.Amount = vendorCredit.TotalAmt;
             line.AmountSpecified = true;
-
             List<LinkedTxn> LinkedTxnList = new List<LinkedTxn>();
             LinkedTxn linkedTxn = new LinkedTxn();
             linkedTxn.TxnId = vendorCredit.Id;
             linkedTxn.TxnType = TxnTypeEnum.VendorCredit.ToString();
             LinkedTxnList.Add(linkedTxn);
             line.LinkedTxn = LinkedTxnList.ToArray();
-
             lineList.Add(line);
-
             billPayment.Line = lineList.ToArray();
 
             return billPayment;
         }
-
     }
 }
