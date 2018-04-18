83cae8b70ba0d568ae607f28ab185dfd8d2043f3
Manish Khobragade <manish_khobragade@intuit.com>
2018-04-17 22:46:46 -0700

    Invoice and payment creation in QBO

--- MvcCodeFlowClientManual/Controllers/InvoicingController.cs
+++ MvcCodeFlowClientManual/Controllers/InvoicingController.cs
@@ -37,22 +37,37 @@ namespace MvcCodeFlowClientManual.Controllers
 
                 try
                 {
+
                     
+
                     //Initialize OAuth2RequestValidator and ServiceContext
                     ServiceContext serviceContext = base.IntializeContext(realmId);
                     DataService dataService = new DataService(serviceContext);
 
+                    //Create Account 
+                    Account account = CreateAccount();
+                    Account accountAdded =  dataService.Add<Account>(account);
 
                     //Add customer
+                    Customer customer = CreateCustomer();
+                    Customer customerCreated = dataService.Add<Customer>(customer);
+
                     //Add item
+                    Item item = CreateItem(accountAdded);
+                    Item itemAdded = dataService.Add<Item>(item);
+
                     //Add Invoice
-                    Invoice objInvoice = CreateInvoice(realmId);
+                    Invoice objInvoice = CreateInvoice(realmId, customerCreated, itemAdded);
                     Invoice addedInvoice = dataService.Add<Invoice>(objInvoice);
                     //Email invoice
-                    //Download pdf for Invoice
+                    
+                    // sending invoice 
+                    dataService.SendEmail<Invoice>(addedInvoice, "manishk73@gmail.com");
+
                     //Recieve payment for this invoice
 
-
+                   Payment payment = CreatePayment(customerCreated, addedInvoice);
+                   dataService.Add<Payment>(payment);
 
                     return View("Index", (object)("QBO API calls Success!"));
                 }
@@ -66,24 +81,124 @@ namespace MvcCodeFlowClientManual.Controllers
                 return View("Index", (object)"QBO API call Failed!");
         }
 
+
+        private Account CreateAccount()
+        {
+
+            Random randomNum = new Random();
+            Account account = new Account();
+
+         
+            account.Name = "Name_" + randomNum.Next();
+
+            account.FullyQualifiedName = account.Name;
+
+            account.Classification = AccountClassificationEnum.Revenue;
+            account.ClassificationSpecified = true;
+            account.AccountType = AccountTypeEnum.Bank;
+            account.AccountTypeSpecified = true;
+
+            account.CurrencyRef = new ReferenceType()
+            {
+                name = "United States Dollar",
+                Value = "USD"
+            };
+
+            return account;
+        }
+        /// <summary>
+        /// This API creates invoice item 
+        /// </summary>
+        /// <returns></returns>
+        private Item CreateItem(Account incomeAccount)
+        {
+           
+            Item item = new Item();
+
+            Random randomNum = new Random();
+
+            item.Name = "Replacement of Item-" + randomNum.Next();
+            item.Description = "Description";
+            item.Type = ItemTypeEnum.NonInventory;
+            item.TypeSpecified = true;
+
+            item.Active = true;
+            item.ActiveSpecified = true;
+
+
+            item.Taxable = false;
+            item.TaxableSpecified = true;
+
+            item.UnitPrice = new Decimal(100.00);
+            item.UnitPriceSpecified = true;
+
+            item.TrackQtyOnHand = false;
+            item.TrackQtyOnHandSpecified = true;
+
+          
+            item.IncomeAccountRef = new ReferenceType()
+            {
+                name = incomeAccount.Name,
+                Value = incomeAccount.Id
+            };
+
+            item.ExpenseAccountRef = new ReferenceType()
+            {
+                name = incomeAccount.Name,
+                Value = incomeAccount.Id
+            };
+            return item;
+
+        }
+
+       
+        /// <summary>
+        /// This API creates customer 
+        /// </summary>
+        /// <returns></returns>
+        private Customer CreateCustomer()
+        {
+            Random random = new Random();
+            Customer customer = new Customer();
+            customer.AcctNum = "221121" + random.Next();
+            customer.Active = true;
+            customer.AltContactName = "Another Contact ";
+            TelephoneNumber telephone = new TelephoneNumber();
+            telephone.FreeFormNumber = "510-291-9831";
+            customer.AlternatePhone = telephone;
+            customer.Balance = 1000;
+            customer.CompanyName = "Water works " + random.Next();
+            customer.ContactName = "Bob Sterling";
+            customer.CreditLimit = 2000;
+            customer.CreditLimitSpecified = true;
+            customer.domain = "www.waterworks.com";
+            customer.TDSEnabled = true;
+            customer.TDSEnabledSpecified = true;
+            customer.GivenName = "Bob" + random.Next();
+            customer.FamilyName = "Serling";
+            customer.DisplayName = customer.CompanyName;
+            return customer;
+        }
+       
+
         /// <summary>
         /// This API creates an Invoice
         /// </summary>
-        private Invoice CreateInvoice(string realmId)
+        private Invoice CreateInvoice(string realmId, Customer customer, Item item)
         {
-
+            
 
             // Step 1: Initialize OAuth2RequestValidator and ServiceContext
             ServiceContext serviceContext = IntializeContext(realmId);
 
             // Step 2: Initialize an Invoice object
             Invoice invoice = new Invoice();
-            invoice.Deposit = new Decimal(0.00);
-            invoice.DepositSpecified = true;
+           // invoice.Deposit = new Decimal(0.00);
+            //invoice.DepositSpecified = true;
 
             // Step 3: Invoice is always created for a customer so lets retrieve reference to a customer and set it in Invoice
-            QueryService<Customer> querySvc = new QueryService<Customer>(serviceContext);
-            Customer customer = querySvc.ExecuteIdsQuery("SELECT * FROM Customer WHERE CompanyName like 'Amy%'").FirstOrDefault();
+            /*QueryService<Customer> querySvc = new QueryService<Customer>(serviceContext);
+            Customer customer = querySvc.ExecuteIdsQuery("SELECT * FROM Customer WHERE CompanyName like 'Amy%'").FirstOrDefault();*/
             invoice.CustomerRef = new ReferenceType()
             {
                 Value = customer.Id
@@ -91,15 +206,13 @@ namespace MvcCodeFlowClientManual.Controllers
 
 
             // Step 4: Invoice is always created for an item so lets retrieve reference to an item and a Line item to the invoice
-            QueryService<Item> querySvcItem = new QueryService<Item>(serviceContext);
-            Item item = querySvcItem.ExecuteIdsQuery("SELECT * FROM Item WHERE Name = 'Lighting'").FirstOrDefault();
+           /* QueryService<Item> querySvcItem = new QueryService<Item>(serviceContext);
+           Item item = querySvcItem.ExecuteIdsQuery("SELECT * FROM Item WHERE Name = 'Lighting'").FirstOrDefault();*/
             List<Line> lineList = new List<Line>();
             Line line = new Line();
             line.Description = "Description";
             line.Amount = new Decimal(100.00);
             line.AmountSpecified = true;
-            lineList.Add(line);
-            invoice.Line = lineList.ToArray();
 
             SalesItemLineDetail salesItemLineDetail = new SalesItemLineDetail();
             salesItemLineDetail.Qty = new Decimal(1.0);
@@ -111,6 +224,9 @@ namespace MvcCodeFlowClientManual.Controllers
 
             line.DetailType = LineDetailTypeEnum.SalesItemLineDetail;
             line.DetailTypeSpecified = true;
+
+            lineList.Add(line);
+            invoice.Line = lineList.ToArray();
 
             // Step 5: Set other properties such as Total Amount, Due Date, Email status and Transaction Date
             invoice.DueDate = DateTime.UtcNow.Date;
@@ -133,8 +249,43 @@ namespace MvcCodeFlowClientManual.Controllers
                 TotalTax = Convert.ToDecimal(10),
                 TotalTaxSpecified = true,
             };
-
             return invoice;
+        }
+
+    /// <summary>
+    /// Creating payment transaction 
+    /// </summary>
+    /// <param name="customer"></param>
+    /// <param name="invoiceCreated"></param>
+    /// <returns></returns>
+      private  Payment CreatePayment(Customer customer, Invoice invoiceCreated)
+       {
+            Payment payment = new Payment();
+            payment.CustomerRef = new ReferenceType
+            {
+                name = customer.DisplayName,
+                Value = customer.Id
+            };
+            payment.CurrencyRef = new ReferenceType {
+                   type = "Currency",
+                   Value = "USD"
+            };
+            payment.TotalAmt = invoiceCreated.TotalAmt;
+            payment.TotalAmtSpecified = true;
+
+            List<LinkedTxn> linkedTxns = new List<LinkedTxn>();
+            linkedTxns.Add(new LinkedTxn() {
+                TxnId = invoiceCreated.Id,
+                TxnType = TxnTypeEnum.Invoice.ToString()
+            });
+
+             foreach(Line line in invoiceCreated.Line)
+             {
+                 line.LinkedTxn = linkedTxns.ToArray();
+             }
+
+            payment.Line = invoiceCreated.Line;
+            return payment;
         }
     }
 }
